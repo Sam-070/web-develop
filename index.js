9c1bad8e5c5dfdf7e0179f64f4914c9b8d0fcaee
saman <samanabid8715@gmail.com>
2023-07-19 11:02:05 +0530

    js code

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log('hello javascript');
+num=87;
+console.log(num);
+message='Nice';
+console.log(message);
+console.log(typeof num);
+console.log(typeof message);
+
+age=30;
+if(age>=18){
+    console.log("Eligible!");
+}
+else{
+    console.log("Not Eligible!");
+}
