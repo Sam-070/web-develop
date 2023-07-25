151086e4fcc963fd5d27545719c563516976b6a1
saman <samanabid8715@gmail.com>
2023-07-25 11:08:14 +0530

    neww

--- objects.js
+++ objects.js
@@ -1,25 +1,56 @@
-const user = {name:'Raju', email:'raju@email', password:'9876'};
+// const user = {name:'Raju', email:'raju@email', password:'9876'};
 
-console.log(user.name);
-console.log(user['email']);
+// console.log(user.name);
+// console.log(user['email']);
 
-user.address= 'Lucknow';
-console.log(user);
+// user.address= 'Lucknow';
+// console.log(user);
 
-user.password= '1234';
-console.log(user);
+// user.password= '1234';
+// console.log(user);
 
-let customkey= 'email';
-console.log(user[customkey]);
+// let customkey= 'email';
+// console.log(user[customkey]);
 
-let smartphone = {
-    brand : 'Samsung',
-    model : 'S21',
-    price : 72000,
-    color : 'Black',
-};
-smartphone.color = ['Black', 'White', 'Blue'];
-console.log(smartphone);
-console.log(smartphone.color[2]);
-smartphone.color.push('Red');
-console.log(smartphone);
+// let smartphone = {
+//     brand : 'Samsung',
+//     model : 'S21',
+//     price : 72000,
+//     color : 'Black',
+// };
+// smartphone.color = ['Black', 'White', 'Blue'];
+// console.log(smartphone);
+// console.log(smartphone.color[2]);
+// smartphone.color.push('Red');
+// console.log(smartphone);
+
+
+const smartphoneList= [
+    { brand:'Samsung', model:'S21', price: 70000, color: ['Black','Blue']},
+    { brand:'Samsung', model:'S23', price: 82000, color: ['Black','Blue']},
+    { brand:'Apple', model:'iPhone 12', price: 80000, color: ['White','Red','Yellow']},
+    { brand:'OnePlus', model:'9 Pro', price: 60000, color: ['Blue','White']},
+    { brand:'OnePlus', model:'Nord', price: 40000, color: ['Blue','White']},
+    { brand:'Xiaomi', model:'Mi 11', price: 50000, color: ['Black','Gold']},
+];
+
+// console.log(smartphoneList[2].price);
+
+// smartphoneList[3].color.push('Blue');
+// console.log(smartphoneList[3]);
+
+// smartphoneList[1].color.splice(1);
+// console.log(smartphoneList[1]);
+
+// const filteredResult= smartphoneList.filter((phone) => { return phone.price < 70000});
+// console.log(filteredResult);
+
+// const result = smartphoneList.filter((n) => { return n.brand.toLowerCase().includes('sams'.toLowerCase())});
+// console.log(result);
+
+const blue= smartphoneList.filter((n) => { return n.color.includes('Blue') && n.price <= 70000});
+console.log(blue);
+
+const brands= smartphoneList.map((n) => {return n.brand});
+console.log(new Set(brands));//unique brands with set no duplicates
+
---
+++ practice2.js
@@ -0,0 +1,26 @@
+//filter all names strating with S from array
+const names= ['Saman', 'Samaira','Rachel','Sakshi'];
+const namewithS= names.filter((n) => {return n.startsWith('S')});
+console.log(namewithS);
+
+//filter all perfect squares from array
+const num= [25,64,81,234,12];
+function PerfSqr (n) {
+    return Math.sqrt(n)%1 === 0;
+}
+const perSq= num.filter(PerfSqr);
+console.log(perSq);
+
+//filter all prime numbers from array
+const n=[2,7,4,9,5,13,23,45,17,19];
+const primeno= n.filter(chckPrime);
+if(n==2)
+return true;
+function chckPrime(n){
+    for(let i=2; i<n;i++){
+        if(n%i===0)
+        return false;
+    }
+    return true;
+}
+console.log(primeno);
