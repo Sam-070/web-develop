94ddd31bc1fc5520ed37e883ecaf54323e9e06da
saman <samanabid8715@gmail.com>
2023-08-01 10:58:56 +0530

    events

---
+++ section 2/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
+import EventHandling from './components/EventHandling';
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    
+    <Navbar/>
+
+      <Routes>
+      <Route element={ <Home/> } path='/' />
+      <Route element={ <Home/> } path='home' />
+      <Route element={ <Signup/> } path='signup' />
+      <Route element={ <Login/> } path='login' />
+      <Route element={ <EventHandling/> } path='event' />
+      </Routes>
+      
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ section 2/frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Homepage</h1>
+      <h1 className='myclass'>My React App</h1>
+     <input type='text'/>
+     <h1 style={{color: 'green',fontSize: 30}}>Using Inline CSS</h1>
+    </div>
+  )
+}
+
+export default Home
