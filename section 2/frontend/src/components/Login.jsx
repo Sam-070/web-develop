17615198a67f18952c786a4a88a7d46762148579
saman <samanabid8715@gmail.com>
2023-08-07 11:05:06 +0530

    formik

--- section 2/frontend/src/components/Login.jsx
+++ section 2/frontend/src/components/Login.jsx
@@ -1,9 +1,41 @@
+import { useFormik } from 'formik';
 import React from 'react'
 
 const Login = () => {
+
+//initialising formik
+  const loginForm = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    onSubmit : ( values ) => { 
+      console.log(values); 
+      //write code to submit form to server
+    }
+  });
+
   return (
     <div>
-    <h1>Login</h1>
+    <div className='w-25'>
+      <div className='card'>
+        <div className='card-body'>
+          <h3 className='text-center'>Login Form</h3>
+          <hr/>
+
+          <form onSubmit={loginForm.handleSubmit}>
+
+            <label htmlFor=''>Email Address</label>
+            <input type='email' className='form-control mb-3' name='email' onChange={loginForm.handleChange} value={loginForm.values.email}/>
+
+            <label htmlFor=''>Password</label>
+            <input type='password' className='form-control mb-3' name='password' onChange={loginForm.handleChange} value={loginForm.values.password}/>
+
+            <button className='btn btn-primary w-100 mt-5'>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
