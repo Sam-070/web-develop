8abb95b043b3477bd0fa0e9df2093a4f936d098b
saman <samanabid8715@gmail.com>
2023-08-09 11:02:19 +0530

    backend

--- section 2/frontend/src/components/Signup.jsx
+++ section 2/frontend/src/components/Signup.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-        <h1>Signup Page</h1>
+
+<form>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" className="form-control" id="email"/>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
     </div>
   )
 }
