fdb60e819ff821ec9073e8448435a218f2434134
saman <samanabid8715@gmail.com>
2023-12-06 23:31:38 +0530

    add

--- demo-project/section 3/Backend/index.js
+++ demo-project/section 3/Backend/index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 //initialize express app
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
